24bfe32ee27d6d304073d2b2dcdfd5fd09d48380
PatrossDev <contactpatross@gmail.com>
2018-10-26 14:25:29 +0100

    header and footer ready

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+        <footer>
+            <section>
+                <article id="copyright">
+                    <p>Copyright &copy; 2018 - Chamemelon Leads. All rights reserved</p>
+                </article>
+            </section>
+        </footer>
+    </body>
+    <script src="javascript/js.js"></script>
+</html>
--- index.php
+++ index.php
@@ -4,13 +4,6 @@
         <main>
 
         </main>
-        <footer>
-            <section>
-                <article id="copyright">
-                    <p>Copyright &copy; 2018 - Chamemelon Leads. All rights reserved</p>
-                </article>
-            </section>
-        </footer>
-    </body>
-    <script src="javascript/js.js"></script>
-</html>
+<?php    
+    require_once("includes/footer.php")
+?>
