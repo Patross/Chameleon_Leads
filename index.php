0b8219cf1538dbd75dd41412ee7c55c62ae53fc1
PatrossDev <contactpatross@gmail.com>
2018-10-26 14:06:14 +0100

    footer in progress

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
     <head>
         <link rel="stylesheet" href="styles/styles.css">
+        <link rel="stylesheet" href="styles/footer.css">
     </head>
     <body>
         <header>
@@ -15,7 +16,11 @@
 
         </main>
         <footer>
-
+            <section>
+                <article id="copyright">
+                    <p>Copyright &copy; 2018 - Chamemelon Leads. All rights reserved</p>
+                </article>
+            </section>
         </footer>
     </body>
     <script src="javascript/js.js"></script>
