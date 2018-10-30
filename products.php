c4cc0ed37fa45443fc2ae8ba49ec95f263b03af3
Patross <contact@patrossdev.com>
2018-10-30 17:58:00 +0000

    placeholder product in progress

--- products.php
+++ products.php
@@ -2,7 +2,14 @@
     require_once("includes/header.php")
 ?>
 <main>
-    <h1>hi ok</h1>
+    <section class="product">
+        <img src="https://via.placeholder.com/150" alt="lead image"></img>
+
+        <article class="product-info">
+            <h3 class="product-name">Lead number 1</h3>
+            <p class=" product-description">This is a very nice description of the product.</p>
+        </article>
+    </section>
 </main>
 <?php 
     require_once("includes/footer.php")
