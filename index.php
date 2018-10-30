004f38a72f1fb427122712171c57ddba7810bcad
John Heath <jc.heath@hotmail.com>
2018-10-30 17:46:18 +0000

    Fixed the nav bar. Made responsive. Organized the files. Proper linkages. Beautified code. More optimizations. Added transitions. Fixed background scrolling.

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
-<?php    
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link type="text/css" rel="stylesheet" href="styles/header.css">
+    <link rel="stylesheet" href="styles/styles.css" type="text/css">
+    <link rel="stylesheet" href="styles/footer.css" type="text/css">
+</head>
+
+</html>
+<?php
     require_once("includes/header.php")
 ?>
 <main>
-    <div>
-        <h1>hi ok</h1>
-    </div>
+    Hi
 </main>
 <?php    
     require_once("includes/footer.php")
