29591dd8ff7b43ec0cea5b04078019797ffb40cf
Patross <contact@patrossdev.com>
2018-10-30 18:30:15 +0000

    moved css links from index to includes/header.php

--- index.php
+++ index.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html>
 
-<head>
-    <link type="text/css" rel="stylesheet" href="styles/header.css">
-    <link rel="stylesheet" href="styles/styles.css" type="text/css">
-    <link rel="stylesheet" href="styles/footer.css" type="text/css">
-</head>
-
-</html>
 <?php
     require_once("includes/header.php")
 ?>
