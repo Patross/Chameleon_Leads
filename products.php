cedd691ffe152c212e3491122fef8a5a1b606cc7
John Heath <jc.heath@hotmail.com>
2018-11-01 22:07:16 +0000

    `

--- products.php
+++ products.php
@@ -2,31 +2,20 @@
     require_once("includes/header.php")
 ?>
 <!DOCTYPE html>
-<html>
-
-<head>
-    <link rel="stylesheet" href="/styles/styles.css">
-</head>
-
-<body>
-    <main>
-        <section class="product">
-           <article class="img-container">
-               <img class="product-img" src="https://via.placeholder.com/150" alt="lead image">
-               <!-- <p class="product-img">Some text (image)</p> -->
-           </article>
-            <article class="product-container">
-                <h3 class="product-name">Lead number 1</h3>
-                <p class=" product-description">This is a very nice description of the product.This is a very nice description of the product.
+<main>
+    <section class="product">
+        <article class="img-container">
+            <img class="product-img" src="https://via.placeholder.com/150" alt="lead image">
+            <!-- <p class="product-img">Some text (image)</p> -->
+        </article>
+        <article class="product-container">
+            <h3 class="product-name">Lead number 1</h3>
+            <p class=" product-description">This is a very nice description of the product.This is a very nice description of the product.
                 <br><br>
                 This is a very nice description of the product.This is a very nice description of the product.</p>
-            </article>
-        </section>
-
-    </main>
-</body>
-
-</html>
+        </article>
+    </section>
+</main>
 <?php 
     require_once("includes/footer.php")
 ?>
