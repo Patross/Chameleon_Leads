8e144b3501cf5d3e4ed6d80462beeccb42b952a3
Patross <contact@patrossdev.com>
2018-11-02 13:01:49 +0000

    Products now get grabbed ffrom the database. Fixed background issue, New issue found - when many products, you can not scroll down to see all of them

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -16,7 +16,29 @@ try
                     grant UPDATE on chameleon.* to 'chameleon'@'%';
                     grant CREATE TEMPORARY TABLES on chameleon.* to 'chameleon'@'%';
                     FLUSH PRIVILEGES;");
+
     $conn->query("use chameleon;");
+
+    $conn->query("CREATE TABLE IF NOT EXISTS users(
+	id int not null primary key auto_increment,
+    first_name varchar(255) not null,
+    last_name varchar(255) not null,
+    username varchar(255) not null,
+    email varchar(255) not null
+    );");
+
+    $conn->query("CREATE TABLE IF NOT EXISTS products(
+	id int not null primary key auto_increment,
+    name varchar(255) not null,
+    description varchar(255) not null
+    );");
+
+    $conn->query("CREATE TABLE IF NOT EXISTS images(
+	id int not null primary key auto_increment,
+    path varchar(255) not null,
+    product_id int not null,
+    foreign key (product_id) references products(id)
+    );");
 }
 catch(PDOException $e)
 {
--- products.php
+++ products.php
@@ -2,18 +2,35 @@
     require_once("includes/header.php")
 ?>
 <main>
-    <section class="product">
+    <!--<section class="product">
         <article class="img-container">
-            <img class="product-img" src="https://via.placeholder.com/150" alt="lead image">
+            <img class="product-img" src="https://via.placeholder.com/1080" alt="lead image">
             <!-- <p class="product-img">Some text (image)</p> -->
-        </article>
+       <!-- </article>
         <article class="product-container">
             <h3 class="product-name">Lead number 1</h3>
             <p class=" product-description">This is a very nice description of the product.This is a very nice description of the product.
                 <br><br>
                 This is a very nice description of the product.This is a very nice description of the product.</p>
         </article>
-    </section>
+    </section>-->
+
+    <?php
+        $sql = $conn->query("select * from products");
+        
+        foreach ($sql->fetchAll(PDO::FETCH_ASSOC) as $row) {
+            echo '
+                <section class="product">
+                    <article class="img-container">
+                        <img class="product-img" src="https://via.placeholder.com/1080" alt="lead image">
+                    </article>
+                    <article class="product-container">
+                        <h3 class="product-name">'.$row["name"].'</h3>
+                        <p class=" product-description">'.$row["description"].'</p>
+                    </article>
+                </section>';
+}
+    ?>
 </main>
 <?php 
     require_once("includes/footer.php")
