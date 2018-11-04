c37bb32d16e60ee7e7811758807efea927aec85c
John Heath <jc.heath@hotmail.com>
2018-11-04 01:13:48 +0000

    The "Hi" was a bit boring. So I replaced it with a real-time clock. :/

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     require_once("includes/header.php");
 ?>
 <main>
-    Hi
+    <iframe src="http://free.timeanddate.com/clock/i6hk7tfp/n304/tluk/ftb/th1" frameborder="0" width="59" height="18"></iframe>
 </main>
 <?php    
     require_once("includes/footer.php")
