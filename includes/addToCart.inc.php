c205ab10069cece29a529da4fc5a6876a49b7067
PatrossDev <contactpatross@gmail.com>
2018-11-14 15:56:24 +0000

    adding to the shopping cart backend

--- includes/addToCart.inc.php
+++ includes/addToCart.inc.php
@@ -3,5 +3,7 @@ require_once ("dbh.inc.php");
 session_start();
 
 if(isset($_POST["submit"])){
-    
+    if(isset($_POST["itemid"])){
+        $conn->query(`insert into shopping_cart(item_id,user_id) values($_POST["itemid"],$_SESSION["u_id"])`);
+    }
 }
