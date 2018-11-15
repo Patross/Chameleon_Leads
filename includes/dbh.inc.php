d5415a0aa5e988981e6f4b8a6571a4dcdf8e69b7
PatrossDev <contactpatross@gmail.com>
2018-11-15 18:48:49 +0000

    reee

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,24 +1,24 @@
 <?php
 
-$db_host = "localhost";
+$db_host = "patrossdev.com";
 $db_user = "root";
-$db_pass = "";
+$db_pass = "secretrootpass";
 
 try
 {
-    $conn = new PDO('mysql:host=localhost;dbname=chameleon', $db_user, $db_pass);
-    // $conn = new PDO('mysql:host=localhost;', $db_user, $db_pass);
+    // $conn = new PDO('mysql:host=localhost;dbname=chameleon', $db_user, $db_pass);
+    $conn = new PDO( "mysql:host=patrossdev.com", 'root', 'secretrootpass' );
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // $conn->query("create database if not exists chameleon;");
-    // $conn->query("CREATE USER if not exists 'chameleon'@'%' IDENTIFIED BY 'chameleon-leads';");
-    // $conn->query("  grant SELECT on chameleon.* to 'chameleon'@'%';
-    //                 grant INSERT on chameleon.* to 'chameleon'@'%';
-    //                 grant DELETE on chameleon.* to 'chameleon'@'%';
-    //                 grant UPDATE on chameleon.* to 'chameleon'@'%';
-    //                 grant CREATE TEMPORARY TABLES on chameleon.* to 'chameleon'@'%';
-    //                 FLUSH PRIVILEGES;");
+    $conn->query("create database if not exists chameleon;");
+    $conn->query("CREATE USER if not exists 'chameleon'@'%' IDENTIFIED BY 'chameleon-leads';");
+    $conn->query("  grant SELECT on chameleon.* to 'chameleon'@'%';
+                    grant INSERT on chameleon.* to 'chameleon'@'%';
+                    grant DELETE on chameleon.* to 'chameleon'@'%';
+                    grant UPDATE on chameleon.* to 'chameleon'@'%';
+                    grant CREATE TEMPORARY TABLES on chameleon.* to 'chameleon'@'%';
+                    FLUSH PRIVILEGES;");
 
-    // $conn->query("use chameleon;");
+    $conn->query("use chameleon;");
 
     $conn->query("CREATE TABLE IF NOT EXISTS users(
 	id int not null primary key auto_increment,
