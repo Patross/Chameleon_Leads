1d609e50ffc88fdbe8d11bf6c5345153e2c7ee79
Tamas <30211285@MB306-STU5298>
2018-11-16 15:13:58 +0000

    Can't checkout unless ligged in

--- products.php
+++ products.php
@@ -26,17 +26,19 @@
                     </article>
                     <article class="product-container">
                         <h3 class="product-name">'.$row["name"].'</h3>
-                        <p class=" product-description">'.$row["description"].'</p>
-                            <form action="includes/addToCart.inc.php" method="POST">';
+                        <p class=" product-description">'.$row["description"].'</p>';
                             if(isset($_SESSION['u_id'])){
                                 echo'
+                                <form action="includes/addToCart.inc.php" method="POST">
                                 <button name="submit" class="AddToCart">Add to cart</button>
-                                <input type="text" name="itemid" value="'.$row["id"].'" hidden=hidden />';
+                                <input type="text" name="itemid" value="'.$row["id"].'" hidden=hidden />
+                                </form>';
                             }else{
-                                echo'<a href="login.php"><button>Add to cart</button></a>';
+                                echo'
+                                <form action="login.php" method="POST">
+                                <button name="submit" class="AddToCart">Add to cart</button>
+                                </form>';
                             }
-                            echo'
-                            </form>';
                             if(isset($_GET["added"])&&$_GET["added"]=="true"&&$row['id']==$_GET['itemid']){
                                 echo "<span class=\"added\">item added to cart</span>";
                             }
