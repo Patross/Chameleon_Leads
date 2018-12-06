098776833f1940f8e2c1b7e35bd336fb68fba310
PatrossDev <contactpatross@gmail.com>
2018-12-06 15:20:11 +0000

    cart add remove

--- includes/deleteFromCart.inc.php
+++ includes/deleteFromCart.inc.php
@@ -3,19 +3,15 @@ require_once ("dbh.inc.php");
 session_start();
 
 if(isset($_POST['submit'])){
-    
-    $query = $conn->query("SELECT * FROM shopping_cart WHERE product_id='{$_POST['itemid']}' AND user_id='{$_SESSION['u_id']}' AND amount=0;");
-    $count = $query->rowCount(); //gets count
 
-    if($count<=1){
-        $conn->query("DELETE from shopping_cart WHERE product_id='{$_POST['itemid']}' AND user_id={$_SESSION['u_id']} AND amount=1;");
-    }else{
+    $query = $conn->query("SELECT * FROM shopping_cart WHERE product_id='{$_POST['itemid']}' AND user_id='{$_SESSION['u_id']}';");
 
+    $result = $query->fetch(PDO::FETCH_ASSOC);
+    if($result["amount"] > 1){
         $query = $conn->query("UPDATE shopping_cart SET `amount`=(`amount`-1) WHERE `user_id` = {$_SESSION['u_id']} AND product_id ={$_POST['itemid']};"); 
+    }else{
+        $conn->query("DELETE from shopping_cart WHERE product_id='{$_POST['itemid']}' AND user_id={$_SESSION['u_id']} AND amount=1;");
+
+        header("Location: ../".$_SESSION["lastpage"]);
     }
-
-    
-    
-    header("Location: ../".$_SESSION["lastpage"]);
-
 }
