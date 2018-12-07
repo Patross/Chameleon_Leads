b384226b9f6b7cac01ed71e039cc2d29194173aa
PatrossDev <contactpatross@gmail.com>
2018-12-07 16:20:45 +0000

    Delete from cart redirect

--- includes/deleteFromCart.inc.php
+++ includes/deleteFromCart.inc.php
@@ -14,6 +14,6 @@ if(isset($_POST['submit'])){
         echo'<script type="text/javascript" href=../javascript/js.js>
             ShopToggle();
             </script>';
+        }
         header("Location: ../".$_SESSION["lastpage"]);
-    }
 }
