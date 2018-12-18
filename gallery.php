f7b09c16c064dfcaac118ae1cf224d0668bd41e5
tamas mate <tamasmate48@gmail.com>
2018-12-18 09:13:49 +0000

    stuff

--- gallery.php
+++ gallery.php
@@ -6,11 +6,16 @@
     if(!empty($_SESSION['u_email'])&&$_SESSION['u_email']=="admin@admin.admin"){
         $sql = $conn->query("select * from gallery");
         foreach($sql->fetchAll(PDO::FETCH_ASSOC) as $row){
-            echo'<img src="'.$row['image_path'].'" alt="GalleryImage">';
+            echo'
+            <img src="'.$row['image_path'].'" alt="GalleryImage" class="imgGallery">
+            <form action="includes/removegallery.inc.php" method="POST">
+                <input type="text" name="imageid" hidden=hidden value="'.$row['id'].'"></input>
+                <input type="submit" name="submit" value="Remove Image"></input>
+            </form>';
         }
     }
 ?>
-    <form action="includes/gallery.inc.php" method="POST" enctype="multipart/form-data">
+    <form id="formGallery" action="includes/gallery.inc.php" method="POST" enctype="multipart/form-data">
         <input type="file" name="Image" multiple>
         <input type="text" name="caption" placeholder="Caption">
         <input type="submit" name="submit" value="Add Image">
--- includes/gallery.inc.php
+++ includes/gallery.inc.php
@@ -18,7 +18,8 @@
     // caption varchar(255),
     // verified boolean not null
     
-    $filePath = "img/gallery/".$name.".".$ext;
+    $id = $conn->query("SELECT id FROM gallery ORDER BY id DESC LIMIT 1");
+    $filePath = "img/gallery/".$id.".".$ext;
     $conn->query("insert into gallery(image_path,caption,verified) values('{$filePath}','{$_POST["caption"]}',false)");
 
 
@@ -26,6 +27,7 @@
     if (!file_exists($path)) {
         mkdir($path, 0777, true);
     }
+
 
     $target_file = $path . //productIDhere);hallo
     $uploadOk = 1;
@@ -36,7 +38,7 @@
         if($check !== false) {
             // echo "File is an image - " . $check["mime"] . ".";
             $uploadOk = 1;
-            move_uploaded_file($_FILES["Image"]["tmp_name"], "../img/gallery/".$name.".".$ext);
+            move_uploaded_file($_FILES["Image"]["tmp_name"], "../img/gallery/".$id.".".$ext);
         } else {
             // echo "File is not an image.";
             $uploadOk = 0;
