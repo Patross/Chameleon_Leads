75fe495ea62d1983cc3df6931000a7642e9363e4
PatrossDev <contactpatross@gmail.com>
2018-12-21 23:33:49 +0000

    server

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -6,15 +6,15 @@ $db_pass = "chameleon_leads";
 {
     $conn = new PDO("mysql:host={$db_host};dbname=chameleon",$db_user,$db_pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // $conn->query("create database if not exists chameleon;");
-    // $conn->query("CREATE USER if not exists 'chameleon'@'%' IDENTIFIED BY 'chameleon-leads';");
+    $conn->query("create database if not exists chameleon;");
+    $conn->query("CREATE USER if not exists 'chameleon'@'%' IDENTIFIED BY 'chameleon-leads';");
     // $conn->query("  grant SELECT on chameleon.* to 'chameleon'@'%';
     //                 grant INSERT on chameleon.* to 'chameleon'@'%';
     //                 grant DELETE on chameleon.* to 'chameleon'@'%';
     //                 grant UPDATE on chameleon.* to 'chameleon'@'%';
     //                 grant CREATE TEMPORARY TABLES on chameleon.* to 'chameleon'@'%';
     //                 FLUSH PRIVILEGES;");
-    // $conn->query("use chameleon;");
+    $conn->query("use chameleon;");
 
     $conn->query("CREATE TABLE IF NOT EXISTS users(
 	id int not null primary key auto_increment,
