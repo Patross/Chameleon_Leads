d009ad171d2e9c8e875a266862ccf15de47811e5
PatrossDev <contactpatross@gmail.com>
2018-12-22 14:48:40 +0000

    server

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
     require_once("dbh.inc.php");
     session_start();
     $_SESSION["lastpage"] = basename($_SERVER["PHP_SELF"]);
